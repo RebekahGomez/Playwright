9aa6ee665158d7c888e303db7c0708e78bcda048
Rebekah <rebekahmenn@gmail.com>
2024-06-11 21:45:46 -0400

    created 3 reusable methods

--- src/main/ReusableActions.ts
+++ src/main/ReusableActions.ts
@@ -1,4 +1,5 @@
 import { Page } from '@playwright/test'
+import exp from 'constants'
 
 // navigating to any website reusable method
 export async function navigate(page: Page, url: string) {
@@ -29,4 +30,22 @@ export async function captureText(page:Page, xpath:string, elementName:string) {
 export async function submit(page: Page) {
   console.log("Pressing the Enter button")
   await page.keyboard.press("Enter")
+}
+
+// hover reusable method
+export async function hover(page: Page, xpath: string, elementName: string) {
+  console.log("Hovering over " + elementName)
+  await page.locator(xpath).hover()
+}
+
+// scroll by pixels reusable method
+export async function scrollPixels(page:Page, x:number, y:number) {
+  console.log("Scrolling " + x + ", " + y + " pixels")
+  await page.mouse.wheel(x,y)
+}
+
+// click by index reusable method
+export async function clickByIndex(page:Page, xpath:string, index:number, elementName:string) {
+  console.log("Clicking on " + elementName + " at index " + index)
+  await page.locator(xpath).nth(index).click()
 }
