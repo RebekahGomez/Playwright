f1edc8cd37b1a5ea63df06835b81d452a1501fe0
Rebekah <rebekahmenn@gmail.com>
2024-06-11 20:50:57 -0400

    bing search file

---
+++ src/test/Playwright_Day3_06112024/T1_BingSearch_Reusable.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect, Page } from '@playwright/test'
+import { captureText, click, navigate, sendKeys } from '../../main/ReusableActions'
+
+let page: Page
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage()
+})
+
+test("Search for a keyword on Bing", async () => {
+  // navigate to Bing homepage
+  await navigate(page, "https://www.bing.com/")
+  // enter a keyword in the search field
+  await sendKeys(page, "//*[@name='q']", "playwright", "Search Field")
+  // click on search icon
+  await click(page, "//*[@id='search_icon']", "Search Icon")
+})
+
+test("Capture the search result", async () => {
+  // capture the search result
+  let result = await captureText(page, "//*[@id='b_tween']", "Search Result")
+  let arrayResult = result.split(" ")
+  console.log("The search result is " + arrayResult[1])
+})
