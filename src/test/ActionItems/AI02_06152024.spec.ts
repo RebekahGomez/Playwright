74ddb1ae4c0dfdb90fd777b01408303a1824b0f4
Rebekah <rebekahmenn@gmail.com>
2024-06-13 19:53:45 -0400

    weight watchers action item

--- src/test/ActionItems/AI02_06152024.spec.ts
+++ src/test/ActionItems/AI02_06152024.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, Page } from '@playwright/test'
-import { captureText, click, clickByIndex, hover, hoverByIndex, navigate, scrollPixels } from '../../main/ReusableActions'
+import { captureText, click, clickByIndex, hover, hoverByIndex, navigate, scrollPixels, sendKeys } from '../../main/ReusableActions'
 
 let page: Page
 
@@ -10,8 +10,6 @@ test.beforeAll(async ({ browser }) => {
 test("USPS stamps", async () => {
   await navigate(page, "https://www.usps.com/")
   // hover over Shop module
-  // await page.locator("xpath=//*[text()='Shop']").nth(0).hover()
-  // await hover(page, "xpath=//*[text()='Shop']", "Hover over Shop")
   await hoverByIndex(page, "xpath=//*[text()='Shop']", "Hover over Shop", 0)
   // click on Stamps
   await click(page, "xpath=//*[text()='Stamps']", "Click on Stamps")
@@ -28,6 +26,39 @@ test("USPS stamps", async () => {
   // capture the stamp information and print it
   let result = await captureText(page, "xpath=//*[@class='prod-item-title']", "Capture stamp information")
   // trim and replace multiple spaces with a single space
-  result = result.replace(/\s+/g, ' ').trim()
+  // result = result.replace(/\s+/g, ' ').trim()
+  result = result.trim()
   console.log("The stamp information is: " + result)
+})
+
+
+test("Weight Watchers", async () => {
+
+  // declare array list with zip codes
+  let zipCodes = Array<string>()
+  zipCodes.push("11420")
+  zipCodes.push("60007")
+  
+  // initialize a for loop
+  for (let i = 0; i < zipCodes.length; i++) {
+    await navigate(page, "https://www.weightwatchers.com/us/find-a-workshop/")
+    // click on Find a Workshop
+    await clickByIndex(page, "xpath=//*[@class = 'menuItemIconWrapper-INk4b']", 0, "Find a Workshop btn")
+    // click on In Person
+    await click(page, "xpath=//*[@class = 'studioIcon-mbBcr']", "In Person Btn")
+    // enter the zip code from array list
+    await sendKeys(page, "xpath=//*[@id='location-search']", zipCodes[i], "Zip Code Location Search")
+    //click the Search array
+    await click(page, "//*[@class = 'rightArrow-C_sUu']", "Click Search Arrow")
+    // click by index the first studio link
+    await clickByIndex(page, "//*[@class = 'container-k2b9Z']", 0, "Click Studio Link")
+    // store the address for the studio in a variable and print
+    let studioAddress = await captureText(page, "//*[@class = 'address-FnT8k']", "Studio Address")
+    console.log("The Studio Address is: " + studioAddress)
+    // scroll to schedule
+    await scrollPixels(page, 0, 500)
+    // store the workshop table in a variable and print
+    let workshopTable = await captureText(page, "//*[@class = 'workshopSchedule-2zUZD']", "Workshop Table")
+    console.log(workshopTable)
+  }
 })
